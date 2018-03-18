6d7b57f792ef4a5b2b3f2124b086a219da71ecc5
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2018-03-18 20:19:21 +0100

    handled unknown message types

--- src/index.ts
+++ src/index.ts
@@ -188,6 +188,9 @@ function write(buffer: Buffer, value: any, desc: FrameDescription<any>, fullFram
             return buffer;
         case 'subFrame':
             value[desc.name] = {};
+            if (value[(desc as SubFrameDescription<any, any>).choose.discriminator] in (desc as SubFrameDescription<any, any>).choose.subFrame)
+                return buffer;
+
             buffer = (desc as SubFrameDescription<any, any>).choose.subFrame[value[(desc as SubFrameDescription<any, any>).choose.discriminator]].write(value[desc.name]);
             if (typeof ((desc as SubFrameDescription<any, any>).length) == 'undefined')
                 return buffer;
