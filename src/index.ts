be800437e86d86c3d3fd4869027cd56f64307413
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2018-03-22 07:19:49 +0100

    fixed writing bug

--- src/index.ts
+++ src/index.ts
@@ -187,7 +187,6 @@ function write(buffer: Buffer, value: any, desc: FrameDescription<any>, fullFram
 
             return buffer;
         case 'subFrame':
-            value[desc.name] = {};
             if (!(value[(desc as SubFrameDescription<any, any>).choose.discriminator] in (desc as SubFrameDescription<any, any>).choose.subFrame))
                 return buffer;
 
